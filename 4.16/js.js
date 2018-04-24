cbd33d3008edeb82a5b6eddc541f853b133e4fdd
xqm1213114 <37523179+xqm1213114@users.noreply.github.com>
2018-04-24 16:12:45 +0800

    Add files via upload

---
+++ 4.16/js.js
@@ -0,0 +1,7 @@
+function showPic(whichpic){
+var source=whichpic.href;
+document.getElementById("placeholder").src=source;
+var text=whichpic.getAttribute("title");
+var description=document.getElementById("description");
+description.firstChild.nodeValue=text;
+}
